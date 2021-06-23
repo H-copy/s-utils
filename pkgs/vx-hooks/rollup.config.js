296dcdab48c5b2134797479f432130e2da7a1b37
1123605771@qq.com <huhaiguo@zkdntech.com>
2021-06-23 14:24:45 +0800

    chore 重构rollup打包

--- pkgs/vx-hooks/rollup.config.js
+++ pkgs/vx-hooks/rollup.config.js
@@ -1,10 +1,12 @@
 import path from 'path'
-import resolve from 'rollup-plugin-node-resolve' // 依赖引用插件
-import commonjs from 'rollup-plugin-commonjs' // commonjs模块转换插件
-import { eslint } from 'rollup-plugin-eslint' // eslint插件
-import ts from 'rollup-plugin-typescript2'
-const getPath = _path => path.resolve(__dirname, _path)
 import packageJSON from './package.json'
+import pluginResolve from '@rollup/plugin-node-resolve' // 依赖引用插件
+import pluginCommonjs from '@rollup/plugin-commonjs' // commonjs模块转换插件
+import pluginEslint from '@rollup/plugin-eslint' // eslint插件
+import pluginTypescript2 from 'rollup-plugin-typescript2' // typescript
+import pluginTerser from 'rollup-plugin-terser' // 压缩
+const getPath = _path => path.resolve(__dirname, _path)
+
 
 const extensions = [
   '.js',
@@ -12,30 +14,78 @@ const extensions = [
   '.tsx'
 ]
 
-const tsPlugin = ts({
-  tsconfig: getPath('./tsconfig.json'),
-  extensions
-})
 
-const esPlugin = eslint({
+const eslintConf = {
   throwOnError: true,
-  include: ['src/**/*.ts'],
-  exclude: ['node_modules/**', 'lib/**', 'src/**/__test__/**']
-})
-
-const commonConf = {
-  input: getPath('./src/index.ts'),
-  external: ['vue'],
-  plugins:[
-    resolve(extensions),
-    commonjs(),
-    esPlugin,
-    tsPlugin,
-  ],
-  output: [
-    { name: packageJSON.name,  globals:{ vue: 'vue' }, file: packageJSON.main, format: 'umd' },
-    { name: packageJSON.name,  globals:{ vue: 'vue' }, file: packageJSON.module, format: 'es' },
-  ]
+  include: ['src/**'],
+  exclude: ['node_modules/**', 'lib/**']
 }
 
-export default commonConf
+const mini = {
+  input: getPath('./src/index.ts'),
+  external: ['vue'],
+  plugins: [
+    pluginEslint(eslintConf),
+    pluginResolve(extensions),
+    pluginCommonjs(),
+    pluginTypescript2({
+      tsconfig: getPath('./tsconfig.mini.json'),
+      extensions
+    }),
+    pluginTerser.terser()
+  ],
+  output:{
+    name: packageJSON.name,
+    file: packageJSON.main,
+    format: 'umd'
+  }
+}
+
+const esm = {
+  input: getPath('./src/index.ts'),
+  external: ['vue'],
+  plugins: [
+    pluginEslint(eslintConf),
+    pluginResolve(extensions),
+    pluginCommonjs(),
+    pluginTypescript2({
+      tsconfig: getPath('./tsconfig.json'),
+      extensions
+    }),
+  ],
+  output:{
+    name: packageJSON.name,
+    file: packageJSON.module,
+    format: 'esm'
+  }
+}
+
+export default [mini, esm]
+
+// const tsPlugin = ts({
+//   tsconfig: getPath('./tsconfig.json'),
+//   extensions
+// })
+
+// const esPlugin = eslint({
+//   throwOnError: true,
+//   include: ['src/**/*.ts'],
+//   exclude: ['node_modules/**', 'lib/**', 'src/**/__test__/**']
+// })
+
+// const commonConf = {
+//   input: getPath('./src/index.ts'),
+//   external: ['vue'],
+//   plugins:[
+//     resolve(extensions),
+//     commonjs(),
+//     esPlugin,
+//     tsPlugin,
+//   ],
+//   output: [
+//     { name: packageJSON.name,  globals:{ vue: 'vue' }, file: packageJSON.main, format: 'umd' },
+//     { name: packageJSON.name,  globals:{ vue: 'vue' }, file: packageJSON.module, format: 'es' },
+//   ]
+// }
+
+// export default commonConf
